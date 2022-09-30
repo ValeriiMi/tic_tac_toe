d5c0a905bf0a61417fc64a6f4df393f86431d837
Valeria <leramoldovanova@mail.ru>
2022-09-30 17:12:46 +0300

    Добавление кода функции check_Victory() и успешное тестирование

--- game.py
+++ game.py
@@ -25,3 +25,16 @@ def check_Tie(playerpos):
     if len(playerpos['X']) + len(playerpos['O']) == 9:
         return True
     return False
+
+
+def check_Victory(playerpos, curplayer):
+    # All probable winning combinations
+    solution = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+
+    # Loop to check whether any winning combination is satisfied or not
+    for i in solution:
+        if all(j in playerpos[curplayer] for j in i):
+            # Return True if any winning combination is satisfied
+            return True
+            # Return False if no combination is satisfied
+    return False
