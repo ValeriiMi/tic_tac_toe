01da5f1c981c8547278a070c064e7ce5f8eb34d5
Valeria <leramoldovanova@mail.ru>
2022-09-30 16:33:49 +0300

    Добавление тестов для функции check_Tie()

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from game import mytictactoe
+from game import mytictactoe, check_Tie
 
 
 class PrimeNumbersTestCase(unittest.TestCase):
@@ -9,3 +9,11 @@ class PrimeNumbersTestCase(unittest.TestCase):
         str1 = "\n\t     |     |\n\t  1  |  2  |  3\n\t_____|_____|_____\n\t     |     |\n\t  4  |  5  |  6\n" \
                "\t_____|_____|_____\n\t     |     |\n\t  7  |  8  |  9\n\t     |     |\n"
         self.assertEqual(result, str1)
+
+    def test_check_Tie1(self):
+        result = check_Tie({'X': [1, 3, 5, 8], 'O': [2, 4, 6, 7, 9]})
+        self.assertEqual(result, True)
+
+    def test_check_Tie2(self):
+        result = check_Tie({'X': [1, 3, 5, 8], 'O': [2, 4, 6, 7]})
+        self.assertEqual(result, False)
